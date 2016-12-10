5c4219be793f77c84dc69f317ded6c2ad77ffb1e
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2016-12-10 15:08:37 +0000

    for each valid move, two boxes are coloured in. Edge cases need to be sorted next

--- js/app.js
+++ js/app.js
@@ -10,26 +10,35 @@ Domineering.start = function() {
   Domineering.createBoard();
 };
 
-
-Domineering.changePlayer = function(){
+Domineering.changePlayer = function() {
   if (this.currPlayer === 'Player A'){
     this.currPlayer = 'Player B';
   } else {
     this.currPlayer = 'Player A';
   }
-  console.log(this.currPlayer);
+  console.log('changed player to ' + this.currPlayer);
 };
 
-Domineering.playMove = function(e){
-  console.log(e.target);
-  console.log('I have been clicked');
-  Domineering.changePlayer();
+Domineering.playMove = function(e) {
+  if (e.target.getAttribute('class') === null) {
+    if (this.currPlayer === 'Player A') {
+      e.target.setAttribute('class', 'Aplayed');
+      var secondBoxA = parseInt(e.target.id) + this.base;
+      document.getElementById(secondBoxA).setAttribute('class', 'Aplayed');
+      Domineering.changePlayer();
+    } else {
+      e.target.setAttribute('class', 'Bplayed');
+      var secondBoxB = parseInt(e.target.id) + 1;
+      document.getElementById(secondBoxB).setAttribute('class', 'Bplayed');
+      Domineering.changePlayer();
+    }
+  }
 };
 
 Domineering.addButtonFunctionality = function() {
   var squares = document.getElementsByTagName('li');
   for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', Domineering.playMove);
+    squares[i].addEventListener('click', Domineering.playMove.bind(this));
   }
 };
 
@@ -46,32 +55,3 @@ Domineering.createBoard = function () {
 };
 
 document.addEventListener('DOMContentLoaded', Domineering.start);
-
-// SoundBoard.addLyrics = function() {
-//   var tiles = document.getElementsByTagName('li');
-//   for (var i = 0; i < tiles.length; i++) {
-//     tiles[i].innerHTML = soundClipArray[i].title;
-//   }
-// };
-//
-// SoundBoard.addAudioTag = function() {
-//   var audio = document.createElement('audio');
-//   audio.setAttribute('id', 'audio');
-//
-//   var body = document.getElementsByTagName('body')[0];
-//   body.appendChild(audio);
-// };
-//
-// SoundBoard.addButtonFunctionality = function() {
-//   var tiles = document.getElementsByTagName('li');
-//
-//   for (var i = 0; i < tiles.length; i++) {
-//     tiles[i].addEventListener('click', SoundBoard.assignSoundClip(i));
-//   }
-// };
-//
-// SoundBoard.assignSoundClip = function(i) {
-//   var audio = document.getElementById('audio');
-//   audio.src = soundClipArray[i].wavFile;
-//   audio.play();
-// };
