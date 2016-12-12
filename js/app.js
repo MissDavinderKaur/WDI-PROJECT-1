96396b083c28184dbbb9dbf04e8032913fa6a9e9
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2016-12-12 16:23:44 +0000

    converted some bits to jQuery and refactored code so not repeated therfore game may actually be broken

--- js/app.js
+++ js/app.js
@@ -7,26 +7,24 @@ function Domineering (base) {
   this.currPlayer = 'Player A';
 }
 
+//Change the player
 Domineering.changePlayer = function() {
-  console.log('wanting to change player', this);
   if (game.currPlayer === 'Player A'){
     game.currPlayer = 'Player B';
-    document.getElementById('currentPlayer').innerHTML = 'Current Player: ' + game.playerB;
+    $('#currentPlayer').html('Current Player: ' + game.playerB);
   } else {
     game.currPlayer = 'Player A';
-    document.getElementById('currentPlayer').innerHTML = 'Current Player: ' + game.playerA;
+    $('#currentPlayer').html('Current Player: ' + game.playerA);
   }
 };
 
-// Can Player A/Blue/vertical make a move?
+
+//Check for any 2 boxes (one above another)that are empty. If there are any, then the //user has options and so play can continue.
 Domineering.canPlayerAmove = function() {
   var options = 0;
   for (var i = 0; i < ((this.base * this.base) - this.base); i++) {
-    var classOfIndex = (document.getElementById(i)).getAttribute('class');
-    var possibleSecondBoxB = i + this.base;
-    var classOfSecondBox = (document.getElementById(possibleSecondBoxB)).getAttribute('class');
-
-    if (classOfIndex === null && classOfSecondBox === null) {
+    var possibleSecondBoxA = i + this.base;
+    if (Domineering.checkBoxes(i, possibleSecondBoxA)) {
       options++;
     }
   }
@@ -38,15 +36,12 @@ Domineering.canPlayerAmove = function() {
   }
 };
 
-// Can Player B/Red/Horizonatal make a move?
+//Check for any side-by-side boxes that are empty. IF there are any, then the //user has options and so play can continue.
 Domineering.canPlayerBmove = function() {
   var options = 0;
   for (var i = 0; i < ((this.base * this.base) -1); i++) {
-    var classOfIndex = (document.getElementById(i)).getAttribute('class');
     var possibleSecondBoxB = i + 1;
-    var classOfSecondBox = (document.getElementById(possibleSecondBoxB)).getAttribute('class');
-
-    if (classOfIndex === null && classOfSecondBox === null) {
+    if (Domineering.checkBoxes(i, possibleSecondBoxB)) {
       options++;
     }
   }
@@ -58,14 +53,18 @@ Domineering.canPlayerBmove = function() {
   }
 };
 
-Domineering.changeBoxColor = function(player, square) {
+//Change the colour of the boxes played by the user
+Domineering.changeBoxColor = function(player, square1, square2) {
   if (player === 'Player A'){
-    document.getElementById(square).setAttribute('class', 'Aplayed');
+    $('#' + square1).attr('class', 'Aplayed');
+    $('#' + square2).attr('class', 'Aplayed');
   } else {
-    document.getElementById(square).setAttribute('class', 'Bplayed');
+    $('#' + square1).attr('class', 'Bplayed');
+    $('#' + square2).attr('class', 'Aplayed');
   }
 };
 
+//Using the checkBoxes method, work out if the two boxes are empty. if so //then the move selected by Player A is valid and can be made. Then if Player //A can make a move, change the player otherwise alert winner.
 Domineering.makeBmove = function (e) {
   var unallowedIndexes = [];
   for (var i = 1; i <= this.base; i++) {
@@ -76,35 +75,46 @@ Domineering.makeBmove = function (e) {
     //user is not allowed to make this move
   } else {
     var secondBoxB = parseInt(e.target.id) + 1;
-    if (e.target.getAttribute('class') === null &&
-    document.getElementById(secondBoxB).getAttribute('class') === null) {
-      Domineering.changeBoxColor('Player B', e.target.id);
-      Domineering.changeBoxColor('Player B', secondBoxB);
+
+    if (Domineering.checkBoxes(e.target.id, secondBoxB)) {
+      Domineering.changeBoxColor('Player B', e.target.id, secondBoxB );
       if (Domineering.canPlayerAmove.call(game)){
         Domineering.changePlayer.call(game);
       } else {
-        document.getElementById('currentPlayer').innerHTML = this.playerA + ' cannot make a move. Therefore the winner is ' + this.playerB;
+        $('#currentPlayer').html(this.playerA + ' cannot make a move. Therefore the winner is ' + this.playerB);
       }
     }
   }
 };
 
+//Using the checkBoxes method, work out if the two boxes are empty. if so //then the move selected by Player A is valid and can be made. Then, if //Player B is able to make a move then change the player otherwise alert winner.
 Domineering.makeAmove = function (e) {
   if (e.target.id < ((this.base * this.base) - this.base)) {
     var secondBoxA = parseInt(e.target.id) + this.base;
-    if (e.target.getAttribute('class') === null &&
-    document.getElementById(secondBoxA).getAttribute('class') === null) {
-      Domineering.changeBoxColor('Player A', e.target.id);
-      Domineering.changeBoxColor('Player A', secondBoxA);
+    if (Domineering.checkBoxes(e.target.id, secondBoxA)) {
+      Domineering.changeBoxColor('Player A', e.target.id, secondBoxA);
       if (Domineering.canPlayerBmove.call(game)){
         Domineering.changePlayer.call(game);
       } else {
-        document.getElementById('currentPlayer').innerHTML = this.playerB + ' cannot make a move. Therefore the winner is ' + this.playerA;
+        $('#currentPlayer').html(this.playerB + ' cannot make a move. Therefore the winner is ' + this.playerA);
       }
     }
   }
 };
 
+//internal method to check the classes of the 2 boxes selected by each user
+Domineering.checkBoxes = function (boxOne, boxTwo){
+  var classOfBoxOne = (document.getElementById(boxOne)).getAttribute('class');
+  var classOfSecondBox = (document.getElementById(boxTwo)).getAttribute('class');
+
+  if(classOfBoxOne === null && classOfSecondBox){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//Based on the current player, the user move is made
 Domineering.playMove = function(e) {
   if (this.currPlayer === 'Player A') {
     Domineering.makeAmove.call(game, e);
@@ -113,6 +123,7 @@ Domineering.playMove = function(e) {
   }
 };
 
+//Game play functionality needs to be added to each square
 Domineering.addButtonFunctionality = function() {
   var squares = document.getElementsByTagName('li');
   for (var i = 0; i < squares.length; i++) {
@@ -120,10 +131,11 @@ Domineering.addButtonFunctionality = function() {
   }
 };
 
+//Based on the user input the GUI is generated
 Domineering.createBoard = function () {
-  document.getElementById('leftPlayer').innerHTML = this.playerA;
-  document.getElementById('rightPlayer').innerHTML = this.playerB;
-  document.getElementById('currentPlayer').innerHTML = 'Current player: ' + this.playerA;
+  $('#leftPlayer').html(this.playerA);
+  $('#rightPlayer').html(this.playerB);
+  $('#currentPlayer').html('Current player: ' + this.playerA);
 
   var body = document.getElementById('gameGrid');
   var grid = document.createElement('ul');
@@ -133,31 +145,34 @@ Domineering.createBoard = function () {
     square.setAttribute('id', i);
     grid.appendChild(square);
   }
-  document.getElementsByTagName('ul')[0].style.width = (50 * this.base) + 'px';
-  document.getElementsByTagName('ul')[0].style.height = (50 * this.base) + 'px';
+  $('ul')[0].style.width = (50 * this.base) + 'px';
+  $('ul')[0].style.height = (50 * this.base) + 'px';
 
   Domineering.addButtonFunctionality.call(game);
 };
 
+//Based on the user input, the game is set up (internally)
 Domineering.setupGame = function(e) {
   e.preventDefault();
-  if (document.getElementById('small').checked) {
+  if ($('#small').prop('checked')) {
     game = new Domineering(5);
-  } else if (document.getElementById('medium').checked) {
+  } else if ($('#medium').prop('checked')) {
     game = new Domineering(7);
   } else {
     game = new Domineering(9);
   }
-  game.playerA = document.getElementById('playerAname').value;
-  game.playerB = document.getElementById('playerBname').value;
+  game.playerA = $('#playerAname').val();
+  game.playerB = $('#playerBname').val();
 
-  document.getElementById('userInputForm').style.display = 'none';
+  $('#userInputForm').hide();
 
   Domineering.createBoard.call(game);
 };
 
-Domineering.getPlayersDetails = function() {
-  document.getElementById('userInputForm').addEventListener('submit', Domineering.setupGame);
+//Add event listener to the player input form - they enter usernames and //select the game difficulty
+Domineering.playersDetails = function() {
+  $('#userInputForm').on('submit', Domineering.setupGame);
 };
 
-document.addEventListener('DOMContentLoaded', Domineering.getPlayersDetails);
+//Wait for the page to load
+document.addEventListener('DOMContentLoaded', Domineering.playersDetails);
